75894ad6b1b5b4cf6d1a22437eb3e35a36822930
whfh1359 <whfh1359@ajou.ac.kr>
2021-08-12 17:28:29 +0900

    2021-08-12

--- seyoung/섹션 5/2. 쇠막대기/AA.py
+++ seyoung/섹션 5/2. 쇠막대기/AA.py
@@ -1,3 +1,21 @@
 import sys
 sys.stdin = open("in1.txt", "rt")
 
+a=list(str(input()))
+stack = []
+s=0
+e=0
+count = 0
+for i in range(len(a)):
+    if a[i] =='(':
+        stack.append(a[i])
+        s+=1
+    elif a[i] ==')' and a[i-1] == '(':
+        stack.pop()
+        count += len(stack)
+    elif a[i] == ')' and a[i-1] == ')':
+        stack.pop()
+        count += 1
+print(count)
+
+
