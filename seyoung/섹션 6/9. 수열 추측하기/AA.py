7d6089147a25fc37244b61f89f1acbd932e8e22f
whfh1359 <whfh1359@ajou.ac.kr>
2021-09-15 14:52:20 +0900

    2021-09-15

--- seyoung/섹션 6/9. 수열 추측하기/AA.py
+++ seyoung/섹션 6/9. 수열 추측하기/AA.py
@@ -1,35 +1,24 @@
 import sys
-sys.stdin = open("in1.txt", "rt")
+#sys.stdin = open("in1.txt", "rt")
 
-def DFS(L,total,f):
-    global result
-    if L == n :
-        tmp = []
-        for i in range(len(res)-1):
-            tmp.append(res[i]+res[i+1])
-            print(tmp)
-        # print(sum(tmp[1:-1]))
-        # print(sum(res), sum(tmp[1:-1])*(n-2),f)
-        if (total + sum(tmp[1:-1])*(n-2)) == f:
-            result.append(res[:])
-            return
-        return
-        #     print(1)
-        #     result.append(res)
+def DFS(L,total):
+    if L==n and total==f:
+        for x in p:
+            print(x, end = ' ')
+        sys.exit(0)
     else:
-        for i in range(n):
-            if check[i] == 0:
-                res[L] = i+1
-                check[i] =1
-                total += arr[i]
-                DFS(L+1,total,f)
-                check[i] =0
-                total -= arr[i]
+        for i in range(1, n+1):
+            if ch[i] == 0:
+                ch[i] = 1
+                p[L] = i
+                DFS(L+1, total + (p[L]*b[L]))
+                ch[i] = 0
 if __name__=="__main__":
     n, f = map(int, input().split())
-    arr = list(range(1,n+1))
-    print(arr)
-    res = [0]*n
-    check = [0]*n
-    result = []
-    DFS(0,0,f)
+    p = [0]*n
+    b = [1]*n
+    ch = [0]*(n+1)
+    for i in range(1,n):
+        b[i] = b[i-1]*(n-i)//i
+    # print(b)
+    DFS(0, 0)
