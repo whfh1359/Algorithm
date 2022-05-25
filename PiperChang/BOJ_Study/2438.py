73c0d1e132175fdf728d854250f06a2fded2a255
PiperChang <goddhqj8000@ajou.ac.kr>
2022-05-25 22:15:44 +0900

    2021-08-25

--- PiperChang/BOJ_Study/2438.py
+++ PiperChang/BOJ_Study/2438.py
@@ -1,4 +1,12 @@
-T = int(input())
-for i in range(T):
-    N = int(input())
-    
+def solution(prices) :
+    maxval = 0
+    benefit = -10000000
+    # max value가 바뀌면,
+    for i in prices :
+        benefit = max([maxval - i, benefit])
+        if i > maxval :
+            maxval = i
+
+    return benefit
+
+print(solution([3, 1, 4, 1, 5, 9, 2, 6, 5, 3]))
