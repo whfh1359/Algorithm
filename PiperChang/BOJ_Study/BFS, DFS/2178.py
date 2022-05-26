8ad32dc482aa568fac7a18b1f0f16147b9266d7a
PiperChang <goddhqj8000@ajou.ac.kr>
2022-05-26 16:38:10 +0900

    ㄷㅂㄱㅇ 코테 최장 dir 경로 찾는 문제. 최초 시도는 최상단에서 일일이 최하단으로 dfs 하는 것으로 했으나, 효율성 문제 해결이 안되었음. 그래서 반대로 최 하단 부터 상위로 올라가면서 dfs하고, 해당 dir에서 root까지 갔을때의 길이를 memorization해서 해결함.  root -> leaf로 하게 되면, 하위 node를 전부 봐야 답을 알 수 있지만, leaf -> root로 가면, 한번만 보면 됨
     2178 =>
     matrix 방문한 걸 언제 바꾸는지에 따라서 효율성이 전혀 달라지나 봄.

--- PiperChang/BOJ_Study/BFS, DFS/2178.py
+++ PiperChang/BOJ_Study/BFS, DFS/2178.py
@@ -1,41 +1,75 @@
-N, M  = list(map(int,input().split()))
+# from collections import deque
+#
+# N, M = map(int, input().split())
+#
+# graph = []
+#
+# for _ in range(N):
+#     graph.append(list(map(int, input())))
+#
+#
+# # 너비 우선 탐색
+# def bfs(x, y):
+#     # 이동할 네 가지 방향 정의 (상, 하, 좌, 우)
+#     dx = [-1, 1, 0, 0]
+#     dy = [0, 0, -1, 1]
+#
+#     # deque 생성
+#     queue = deque()
+#     queue.append((x, y, 1))
+#
+#     while queue:
+#         x, y, cnt= queue.popleft()
+#         if x == N-1 and y == M-1:
+#             return cnt
+#
+#         # 현재 위치에서 4가지 방향으로 위치 확인
+#         for i in range(4):
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+#             # 위치가 벗어나면 안되기 때문에 조건 추가
+#             if nx < 0 or nx >= N or ny < 0 or ny >= M:
+#                 continue
+#
+#             # 벽이므로 진행 불가
+#             if graph[nx][ny] == 0:
+#                 continue
+#
+#             # 벽이 아니므로 이동
+#             if graph[nx][ny] == 1:
+#                 graph[nx][ny] = 0
+#                 queue.append((nx, ny, cnt+1))
+#
+#     # 마지막 값에서 카운트 값을 뽑는다.
+#     return cnt
+#
+#
+# print(bfs(0, 0))
 
+from collections import deque
+
+N, M  = list(map(int,input().split()))
 matrix = []
-minval = 100*100
 
 for i in range(N):
-    matrix.append(list(input()))
-visited = matrix
-stack = []
+    matrix.append(list(map(int ,input())))
 
-# dfs의 핵심
-def bfs():
-    # 스택에 넣고 뺴기를 반복. 도착하면 min
-    x = 1
-    y = 1
-    cnt = 1
-    while x != M or y != N :
-        print('ok')
-        print(y<N)
-        print(matrix[x][y+1])
-        if x < M :
-            if matrix[x + 1][y] :
-                matrix[x+1][y] = 0
-                stack.append([x + 1, y])
-        if x > 0 :
-            if  matrix[x - 1][y] :
-                matrix[x - 1][y] = 0
-                stack.append([x - 1, y])
-        if y < N :
-            if matrix[x][y + 1] :
-                matrix[x][y + 1] = 0
-                stack.append([x, y + 1])
-        if y > 0:
-            if matrix[x][y - 1]:
-                matrix[x][y - 1] = 0
-                stack.append([x, y - 1])
-        print(stack)
-        cnt += 1
-        x,y = stack.pop(0)
+def bfs(x,y) :
+    dx = [1, 0, -1, 0]
+    dy = [0, -1,0, 1]
+    queue = deque([(0,0,1)])
 
-bfs()
+    while queue :
+        x,y,cnt = queue.popleft()
+        if x == N - 1 and y == M - 1:
+            return cnt
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0<= nx and nx < N and 0<= ny and ny < M :
+                if matrix[nx][ny] == 1 :
+                    matrix[nx][ny] = 0
+                    queue.append((nx, ny, cnt+1))
+
+print(bfs(1,1))
